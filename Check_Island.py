4574bb7fff057b56672a5a15b2cca3805766f5fa
Mohamed Ashraf <43587732+MohmedAshrf@users.noreply.github.com>
2022-12-19 08:03:01 +0200

    Update and rename check_casi01-02.py to Check_Island.py

--- Check_Island.py
+++ Check_Island.py
@@ -2,9 +2,9 @@ import os 			#to excute commands on OS
 import re 			#using regular expressions to find numbers
 
 def find_Instance(in_line):
-	out= in_line.split(':') 		#i.e casi02-oss5:JBOD_5_6_SLOT_42 will be splitted at the ':'
-	split = out[1].strip() 			#remove all unwanted spaces
-	prefixes = ['JBOD','casi','raidz','mirror'] 
+	out= in_line.split(':') 		#i.e {samplestorageisland}01-oss5:JBOD_5_6_SLOT_42 will be splitted at the ':'
+	split = out[1].strip() 			#Remove all unwanted spaces
+	prefixes = ['JBOD','','raidz','mirror']   #Put all your prefixs here
 	if(any(split.startswith(x) for x in prefixes)):
 		return str(out[1].strip())
 
@@ -15,17 +15,17 @@ def convert_int(in_list):
 	return map(int,in_list) 									 #convert all to integers
 
 os.system('rm -f out.txt')
-cmd = 'psh casi01-mds1-casi02-oss6 "zpool status" > out.txt'	 # find all errors from zpool
-os.system(cmd)													 #excute the command
+cmd = 'psh {samplestorageisland}01-02 "zpool status" > out.txt'	 					# find all errors from zpool
+os.system(cmd)												 #excute the command
 
 Result=[]
 with open("out.txt") as f:
 	for line in f:
-		Instance_line = find_Instance(line)						 #find our targets to check
+		Instance_line = find_Instance(line)							 #find our targets to check
 		if (Instance_line != None):
 			numbers = convert_int(find_nums(Instance_line))
 			if(sum(numbers[-3:])>0): 							 # if errors were found
-				Result.append(line)								 # add to the whole results list
+				Result.append(line)							 # add to the whole results list
 
 if (len(Result)>0):
 	print("Found the following Errors:\n")
